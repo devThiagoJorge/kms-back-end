6ce624eb908c22fe0f03a01669b5045902a23f50
gabriel7731s <gabrielsilva7731@gmail.com>
2019-09-13 15:31:02 -0300

    Token de autenticação sendo gerado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,20 +2,56 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
+const bcryptjs = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
+
 module.exports = {    
   async create(req, res){
     try {
-      const {email} = req.body;
+      const { email } = req.body;
 
-      if (User.findOne({email})) {
-        res.status(400).send({error: 'User already exists!'})  
+      if (await User.findOne({ email })) {
+        return res.status(400).send({error: 'User already exists!'})  
       }
-
-      User.create(req.body);
       
-      res.status(200).send('User created!');
+      const user = User.create(req.body);
+      
+      return res.status(200).send(
+        {user,
+        token: generateToken({ id: user.id })}
+      );
     } catch (error) {
-      res.status(400).send({error: 'User registration failed!'})
+      return res.status(400).send({error: 'User registration failed!'})
     }
+  },
+
+  async authenticate(req, res){
+    const {email, password} = req.body;
+
+    if (!await User.findOne({ email })) { //Se não for encontrado o e-mail (ou seja, se for false, negação de true)
+      return res.status(400).send({error: 'User not found.'});
+    }
+
+    const user = await User.findOne({email}).select('+password'); //.select('+password') seleciona o password do email, pois o mesmo está definido como false no model, ou seja, não é retornado    
+
+    if (!await bcryptjs.compare(password, user.password)) { //O bcrypt.compare verifica se a senha não bate com a senha criptografada
+      return res.status(400).send({error: 'Invalid password.'});
+    }
+
+    user.password = undefined; //Define a senha como undefined, para não ser mostrada para o cliente
+    
+    return res.status(200).send({ 
+      user,
+      token: generateToken({ id: user.id})
+    });
   }
 };
+
+function generateToken(params = {}){
+  return token = jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400 //Expira em 1 dia (86400 segundos)
+  });    
+}

62ae40a61f0401bf10b7b483faa5f74ec4961067
gabriel7731s <gabrielsilva7731@gmail.com>
2019-09-14 11:24:20 -0300

    Conexão com o MongoDB Atlas

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/node-js-learning-project', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/node-js-learning-project',
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+//   }
+// );
+
+mongoose.connect('mongodb+srv://node-js-learning-user:pltADEaEQ6l6USCh@cluster-01-xxwdd.mongodb.net/kennel-manager-system?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true
+  }
+);
